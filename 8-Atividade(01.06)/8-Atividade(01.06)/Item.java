22b2914ad3e1a0e564ed98f581351d55dd2e70da
warl3y <128571569+warl3y@users.noreply.github.com>
2025-06-01 17:38:25 -0300

    Add files via upload

---
+++ 8-Atividade(01.06)/8-Atividade(01.06)/Item.java
@@ -0,0 +1,17 @@
+class Item {
+    private final String nome;
+    private final double preco;
+
+    public Item(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
